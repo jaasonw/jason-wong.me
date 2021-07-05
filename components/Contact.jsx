903f6751ed4e4cadf53f4a1eea65d619ccaa0c40
jaasonw <jayywong92@gmail.com>
2021-07-05 16:52:56 -0700

    cleanup

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,66 +6,55 @@ import { useRouter } from "next/dist/client/router";
 import React from "react";
 import FadeInOnScroll from "./FadeInOnScroll";
 
-export default function Contact() {
+function ContactButton({ icon, text, link }) {
   const router = useRouter();
+  return (
+    <Button
+      margin="5px"
+      leftIcon={icon}
+      bg="#004ECC"
+      _hover={{ bg: "#002766" }}
+      _active={{ bg: "#002766" }}
+      variant="solid"
+      onClick={() => router.push(link)}
+    >
+      {text}
+    </Button>
+  );
+}
+
+export default function Contact() {
   return (
     <Flex w="100%" p={6} bg="#3A86FF" justifyContent="center" color="white">
       <FadeInOnScroll>
         <Container fontSize="xl" maxW="container.lg" textAlign="center">
           <Heading color="#FFBE0B">Contact me</Heading>
-          Interested in hiring me or have an idea to pitch? I&apos;m currently open
-          to work as a frontend developer and looking for opportunities to
-          diversify my portfolio. Let&apos;s get in touch and discuss your projects.
+          Interested in hiring me or have an idea to pitch? I&apos;m currently
+          open to work as a frontend developer and looking for opportunities to
+          diversify my portfolio. Let&apos;s get in touch and discuss your
+          projects.
           <br></br>
           <br></br>
-          <Button
-            margin="5px"
-            leftIcon={<FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>}
-            bg="#004ECC"
-            _hover={{ bg: "#002766" }}
-            _active={{ bg: "#002766" }}
-            variant="solid"
-            onClick={() => router.push("https://www.linkedin.com/in/wayson/")}
-          >
-            LinkedIn
-          </Button>
-          <Button
-            margin="5px"
-            leftIcon={<FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>}
-            bg="#004ECC"
-            _hover={{ bg: "#002766" }}
-            _active={{ bg: "#002766" }}
-            variant="solid"
-            onClick={() => router.push("https://github.com/jaasonw")}
-          >
-            Github
-          </Button>
-          <Button
-            margin="5px"
-            leftIcon={<FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>}
-            bg="#004ECC"
-            _hover={{ bg: "#002766" }}
-            _active={{ bg: "#002766" }}
-            variant="solid"
-            onClick={() =>
-              router.push("mailto:jayywong92@gmail.com")
-            }
-          >
-            Email
-          </Button>
-          <Button
-            margin="5px"
-            leftIcon={<FontAwesomeIcon icon={faFileAlt}></FontAwesomeIcon>}
-            bg="#004ECC"
-            _hover={{ bg: "#002766" }}
-            _active={{ bg: "#002766" }}
-            variant="solid"
-            onClick={() =>
-              router.push("https://jaasonw.github.io/files/resume.pdf")
-            }
-          >
-            Resume
-          </Button>
+          <ContactButton
+            icon={<FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>}
+            text="LinkedIn"
+            link="https://www.linkedin.com/in/wayson/"
+          ></ContactButton>
+          <ContactButton
+            icon={<FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>}
+            text="Github"
+            link="https://github.com/jaasonw"
+          ></ContactButton>
+          <ContactButton
+            icon={<FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>}
+            text="Email"
+            link="mailto:jayywong92@gmail.com"
+          ></ContactButton>
+          <ContactButton
+            icon={<FontAwesomeIcon icon={faFileAlt}></FontAwesomeIcon>}
+            text="Resume"
+            link="https://jaasonw.github.io/files/resume.pdf"
+          ></ContactButton>
           <br></br>
           <br></br>
           Designed and built by Jason Wong
