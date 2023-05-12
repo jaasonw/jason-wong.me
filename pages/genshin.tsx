0287d527a9f3b40dbd5197068608bb23e8e8564c
jaasonw <jayywong92@gmail.com>
2023-05-11 17:06:36 -0700

    GENSHIN

---
+++ pages/genshin.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-7xl">612190698</h1>
+    </div>
+  );
+}
