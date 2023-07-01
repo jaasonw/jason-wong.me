a39ffb621f056dbc9c99e6ffa525df75c9d04a34
jaasonw <jayywong92@gmail.com>
2023-06-30 22:44:30 -0700

    oops king of tiktok has spaces

--- pages/riot_games.tsx
+++ pages/riot_games.tsx
@@ -2,7 +2,7 @@ export default function page() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       <h1 className="text-7xl">wayson#4fun</h1>
-      <h1 className="text-7xl">kingoftiktok#4fun</h1>
+      <h1 className="text-7xl">king of tiktok#4fun</h1>
       <h1 className="text-7xl">kingofwallstreet#stonk</h1>
       <h1 className="text-7xl">i only play league and valorant ngl</h1>
     </div>
