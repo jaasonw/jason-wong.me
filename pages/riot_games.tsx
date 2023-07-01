653ef4c90d68ae9d64f7fcd98bafbc1ff604bcd5
jaasonw <jayywong92@gmail.com>
2023-06-30 21:39:25 -0700

    riot games

---
+++ pages/riot_games.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-7xl">wayson#4fun</h1>
+      <h1 className="text-7xl">kingoftiktok#4fun</h1>
+      <h1 className="text-7xl">kingofwallstreet#stonk</h1>
+    </div>
+  );
+}
