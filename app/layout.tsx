ac1b457963ff20ca9a0cb68aeae2aeeeb509c040
jaasonw <jayywong92@gmail.com>
2023-12-27 22:56:16 -0800

    add umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import "./globals.css";
 
 export default function RootLayout({
@@ -5,5 +7,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Script
+        async
+        src="https://analytics.physicsbirds.dev/script.js"
+        data-website-id="ee179b4d-2e7a-45e9-88d3-b94d9b8175ae"
+      ></Script>
+      {children}
+    </>
+  );
 }
