c4a5c5214f88e6fdf115bbef6302b46c7cfa29b3
jaasonw <jayywong92@gmail.com>
2024-02-06 10:06:10 -0800

    starrail

---
+++ app/(blank)/starrail/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "602266524",
+  description: "602266524",
+};
+export default function Genshin() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-7xl">602266524</h1>
+    </div>
+  );
+}
