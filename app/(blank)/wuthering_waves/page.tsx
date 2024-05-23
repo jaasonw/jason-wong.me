c9e50c4221bc35b8b4aed339999ab0e950097c75
jaasonw <jayywong92@gmail.com>
2024-05-22 19:43:05 -0700

    wuthering waves

---
+++ app/(blank)/wuthering_waves/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "530812212",
+  description: "530812212",
+};
+export default function WutheringWaves() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-7xl">530812212</h1>
+    </div>
+  );
+}
