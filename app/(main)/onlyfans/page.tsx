81e767eff1a6285aca96fd607147cfba9ad9cea9
jason wong <jayywong92@gmail.com>
2025-05-24 23:14:40 -0700

    update redirects to use next native ones

---
+++ app/(main)/onlyfans/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Head from 'next/head'
+import { useEffect } from 'react'
+
+export default function OnlyFans() {
+  useEffect(() => {
+    window.location.href = 'https://gethelp.com/about/'
+  }, [])
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noai, noimageai" />
+        <meta name="twitter:card" content="none" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content=" " />
+        <meta property="og:description" content=" " />
+      </Head>
+      <div>Redirecting...</div>
+    </>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -6,4 +6,83 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/realwayson/",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/wayson/",
+        permanent: true,
+      },
+      {
+        source: "/anilist",
+        destination: "https://anilist.co/user/wayson/",
+        permanent: true,
+      },
+      {
+        source: "/resume",
+        destination: "/resume.pdf",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/jaasonw",
+        permanent: true,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/MwqSCKbUG3",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/realwayson",
+        permanent: true,
+      },
+      {
+        source: "/spotify",
+        destination: "https://open.spotify.com/user/fi14v4phgvmdiqk3g5t7cwsvz",
+        permanent: true,
+      },
+      {
+        source: "/tiktok",
+        destination: "https://www.tiktok.com/@realwayson",
+        permanent: true,
+      },
+      {
+        source: "/donate",
+        destination: "https://ko-fi.com/wayson",
+        permanent: true,
+      },
+      {
+        source: "/youtube",
+        destination: "https://www.youtube.com/channel/UCpHV53n_jlKR4gH3spN8alw",
+        permanent: true,
+      },
+      {
+        source: "/youtubealt",
+        destination: "https://www.youtube.com/channel/UC5zqtuav_Mcu5Jqc5BpRhXA",
+        permanent: true,
+      },
+      {
+        source: "/venmo",
+        destination: "https://venmo.com/code?user_id=2440537361612800300&created=1657495186",
+        permanent: true,
+      },
+      {
+        source: "/zelle",
+        destination: "./zelle.png",
+        permanent: true,
+      },
+      {
+        source: "/meeting",
+        destination: "https://calendly.com/wayson",
+        permanent: true,
+      },
+    ];
+  },
 };
