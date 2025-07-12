1ee49b51072a908e24eb9202875523f5067fe597
jason w <jayywong92@gmail.com>
2025-07-12 03:03:32 -0700

    p

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex flex-col justify-center h-[72vh]">
       <h1 className="text-6xl font-bold">hi, i'm jason</h1>
-      <h2 className="text-2xl">aspiring baker, wip ccna</h2>
+      <h2 className="text-2xl">aspiring baker, wip ccnp</h2>
       <h3 className="text-2xl">
         check out some of what i do{" "}
         <Link
